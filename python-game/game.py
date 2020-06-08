787280bb8225ff4326f9dc892fc7b0a2c961564d
joya <joyanyanwu14@gmail.com>
2020-06-08 15:58:13 +0100

    logic solved

--- python-game/game.py
+++ python-game/game.py
@@ -1,30 +1,29 @@
-#import modeules
-
-
-#options for the game
 import random
-options = ["Rock", "Paper", "Scissors"]
 
+options = ["Rock","Paper","Scissors"]
 computer = random.choice(options)
-    #start the game
 print("Welcome to Rock Paper Scissors!")
-player = input("Type in your choice:")
+user = input("Type in your choice: ")
+user = user.lower()
 
-
-print("Computer says: " + computer)
-
-#logic of the game
-if player == computer:
-    print ("Draw") 
-elif player == "Rock" and computer == "Paper":
-    print("Paper wins!")
-elif player == "Paper" and computer  == "Rock":
-    print("Paper wins!")
-elif player == "Scissors" and computer == "Rock":
-    print("Rock wins!")
-elif player == "Rock" and computer == "Scissors" :
-    print("Rock wins!")
-elif player == "Paper" and computer == "Scissors" :
-    print("Scissors wins!")
-elif player == "Scissors" and computer == "Paper" :
-    print("Scissors wins!")
+if user == 'rock':
+    if computer == 'Rock':
+        print ('Tie Game')
+    elif computer == 'Paper':
+        print ('Computer Wins')
+    else:
+        print ('User Wins')
+if user == 'paper':
+    if computer == 'Rock':
+        print ('User Wins')
+    elif computer == 'Paper':
+        print ('Tie Game')
+    else:
+        print ('Computer Wins')
+if user == 'scissors':
+    if computer == 'Rock':
+        print ('Computer Wins')
+    elif computer == 'Paper':
+        print ('User Wins')
+    else:
+        print ('Tie Game')
